a10ce2245fff8bf9c871b66ff396fdf4c8dba54d
samakunchan <cedric.badjah@gmail.com>
2026-06-02 07:18:33 +0200

    feat: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:signals/signals.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:wiseworkout/core/themes/theme.dart';
 import 'package:wiseworkout/features/cache/services/cache_service.dart';
 import 'package:wiseworkout/features/di/services/service_locator.dart';
 import 'package:wiseworkout/features/helpers.dart';
+import 'package:wiseworkout/features/settings/signals/locale_store.dart';
+import 'package:wiseworkout/features/settings/signals/theme_mode_store.dart';
 import 'package:wiseworkout/features/settings/signals/workout_settings_store.dart';
 import 'package:wiseworkout/l10n/generated/app_localizations.dart';
+import 'package:wiseworkout/layout/screen_scaffold_with_drawer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +20,8 @@ void main() async {
 
   setupLocator();
   await kGetIt<WorkoutSettingsStore>().loadFromCache(kGetIt<CacheService>());
+  await kGetIt<ThemeModeStore>().loadThemeMode();
+  await kGetIt<LocaleStore>().loadLocale();
 
   if (kIsWeb) {
     runApp(
@@ -45,23 +50,32 @@ class _MyAppState extends State<MyApp> {
     final List<Widget> screens = AppHelper.getScreens(pageController: _pageController);
     Widget itemBuilder(BuildContext context, int index) => screens[index];
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: Scaffold(
-        body: SafeArea(
-          child: PageView.builder(
-            controller: _pageController,
-            scrollDirection: .vertical,
-            itemCount: screens.length,
-            onPageChanged: (_) {},
-            itemBuilder: itemBuilder,
+    return SignalBuilder(
+      builder: (BuildContext context) {
+        final ThemeMode themeMode = kGetIt<ThemeModeStore>().themeMode.value;
+        final Locale locale = kGetIt<LocaleStore>().locale.value;
+
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: themeMode,
+          locale: locale,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: ScreenScaffoldWithDrawer(
+            child: SafeArea(
+              child: PageView.builder(
+                controller: _pageController,
+                scrollDirection: .vertical,
+                itemCount: screens.length,
+                onPageChanged: (_) {},
+                itemBuilder: itemBuilder,
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
