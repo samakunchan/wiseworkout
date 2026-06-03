0f2fe2b61a4dd18ff8767bb7bf10293d8a6e0c65
samakunchan <cedric.badjah@gmail.com>
2026-06-03 08:12:42 +0000

    Deploying to gh-pages from @ samakunchan/wiseworkout@a50053600416dd866aac7cae52aa624949219582 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1592404676" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2357015019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
